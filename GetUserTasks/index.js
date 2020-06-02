5d869816cc7e7897974de854fe162a23880e1936
Mats Andreassen <matsand@live.no>
2020-06-02 08:48:57 +0200

    Use userPrincipalName as key, and lookup onPremSamAccountName

--- GetUserTasks/index.js
+++ GetUserTasks/index.js
@@ -4,20 +4,23 @@ const HTTPError = require('../lib/http-error')
 
 module.exports = async (context, req) => {
   try {
-    const samAccountName = req.params.username
-    if (!samAccountName) {
-      throw new HTTPError(400, 'Please pass a username in the url')
+    const userPrincipalName = req.params.userPrincipalName
+    if (!userPrincipalName) {
+      throw new HTTPError(400, 'Please pass the userPrincipalName in the url')
     }
 
-    context.log('tasks', samAccountName)
-    const tasks = await getUserTasks(context, samAccountName)
+    context.log(['tasks', 'get-user-tasks', userPrincipalName, 'get-graph-user'])
+    const graphUser = await getGraphUser(context, { userPrincipalName })
+    context.log(['tasks', 'get-user-tasks', userPrincipalName, 'get-graph-user', graphUser.id])
+
+    context.log(['tasks', 'get-user-tasks', userPrincipalName, 'get-user-tasks'])
+    const tasks = await getUserTasks(context, graphUser)
+    context.log(['tasks', 'get-user-tasks', userPrincipalName, 'tasks', tasks.totalCount])
 
     context.res = {
       status: 200,
       body: {
-        user: {
-          onPremisesSamAccountName: samAccountName
-        },
+        user: graphUser,
         ...tasks
       }
     }
